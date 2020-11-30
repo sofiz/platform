a4fa2ffe6b6347c47fa8cd16c848eb19423aaa55
riyadhriyadh <74514859+riyadhriyadh@users.noreply.github.com>
2020-11-30 15:14:53 +0100

    Update update indexing.php

--- update indexing.php
+++ update indexing.php
@@ -1,6 +1,7 @@
 <?php
  include('conn.php');
  require 'vendor/autoload.php';
+ $obj = new \ArPHP\I18N\Arabic();
 //----------------------- get colomun -------------------------
      $sql1="select * from users ";
 	 $res1=mysqli_query($db,$sql1);
@@ -16,8 +17,8 @@
 		  $words=explode(" ",$row['First_Name']);
 		  foreach ($words as $word) {
 			   if(ord($word)==216 || ord($word)==217)
-									  $soundex .= $obj->soundex($word);
-									  else $soundex .= soundex($word);
+									  $soundex .= " ".$obj->soundex($word);
+									  else $soundex .= " ".soundex($word);
 			  
 
 		                           }
@@ -27,18 +28,57 @@
 		  $words=explode(" ",$row['Last_Name']);
 		  foreach ($words as $word) {
 			   if(ord($word)==216 || ord($word)==217)
-									  $soundex .= $obj->soundex($word);
-									  else $soundex .= soundex($word);
+									  $soundex .= " ".$obj->soundex($word);
+									  else $soundex .= " ".soundex($word);
 
 		                           }
 	                    }
-	  //---------------------------------
+								//---------------------------------
 	  if($row['Username']!=null){
 		  $words=explode(" ",$row['Username']);
 		  foreach ($words as $word) {
 			  if(ord($word)==216 || ord($word)==217)
-									  $soundex .= $obj->soundex($word);
-									  else $soundex .= soundex($word);
+									  $soundex .= " ".$obj->soundex($word);
+									  else $soundex .=" ". soundex($word);
+
+		                           }
+	                    }		//---------------------------------
+	  if($row['Job']!=null){
+		  $words=explode(" ",$row['Job']);
+		  foreach ($words as $word) {
+			  if(ord($word)==216 || ord($word)==217)
+									  $soundex .= " ".$obj->soundex($word);
+									  else $soundex .= " ".soundex($word);
+
+		                           }
+	                    }
+						//---------------------------------
+	  if($row['Wilaya']!=null){
+		  $words=explode(" ",$row['Wilaya']);
+		  foreach ($words as $word) {
+			  if(ord($word)==216 || ord($word)==217)
+									  $soundex .= " ".$obj->soundex($word);
+									  else $soundex .= " ".soundex($word);
+
+		                           }
+	                    }
+						//---------------------------------
+	  if($row['Daira']!=null){
+		  $words=explode(" ",$row['Daira']);
+		  foreach ($words as $word) {
+			  if(ord($word)==216 || ord($word)==217)
+									  $soundex .= " ".$obj->soundex($word);
+									  else $soundex .= " ".soundex($word);
+
+		                           }
+	                    }
+						//---------------------------------
+	  if($row['Commune']!=null){
+		  $words=explode(" ",$row['Commune']);
+		  foreach ($words as $word) {
+			  if(ord($word)==216 || ord($word)==217)
+									  $soundex .= " ".$obj->soundex($word);
+									  else $soundex .= " ".soundex($word);
 
 		                           }
 	                    }
