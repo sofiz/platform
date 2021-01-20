6e4846f83392b93eee8242baa7fd8bee8c0d2a50
sofiz <sofioussama0@gmail.com>
2021-01-21 00:50:43 +0100

    s

--- search.php
+++ search.php
@@ -1,3 +1,56 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+
+<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+<meta name="keywords" content="rondili, service, job, خدمة ,الجزائر ,عامل , رونديلي, بحث">
+<meta name="description" content="
+تبحث عن خدمة أو عامل في منطقتك ؟
+لديك عمل أو خدمة و تبحث عن الزبائن؟
+رونديلي هي الحل، منصة تجمع بين الزبون و أصحاب الأعمال rondili ">
+<meta name="author" content="rondili">
+  <meta charset="utf-8">
+  <title>Rondili - صفحة البحث</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Quicksand:300,500" rel="stylesheet">
+  <link rel="stylesheet" href="search.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+          <script src="https://pagination.js.org/dist/2.1.5/pagination.js"></script>
+       <script src="https://pagination.js.org/dist/2.1.5/pagination.min.js"></script>
+       <script src="pagination-with-styles.js"></script>
+<link href="//db.onlinewebfonts.com/c/7d411bb0357d6fd29347455b7d207995?family=JF+Flat" rel="stylesheet" type="text/css"/>
+
+  <script src="data.js"></script>
+  <script data-ad-client="ca-pub-8433558651734014" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+  <script>
+
+
+function showResult(str) {
+if (str.length==0) {
+  document.getElementById("livesearch").innerHTML="";
+  document.getElementById("livesearch").style.display="none";
+  return;
+}
+var xmlhttp=new XMLHttpRequest();
+xmlhttp.onreadystatechange=function() {
+  if (this.readyState==4 && this.status==200) {
+    document.getElementById("livesearch").innerHTML=this.responseText;
+    document.getElementById("livesearch").style.border="1px solid #A5ACB2";
+    document.getElementById("livesearch").style.display="inline";
+  }
+}
+xmlhttp.open("GET","livesearch.php?q="+str,true);
+xmlhttp.send();
+}
+
+function sugtoinput(x) {
+document.getElementById('searchinput').value=x;
+}
+</script>
+
+</head>
+
+
 <?php
 ini_set('display_errors', 1); ini_set('log_errors',1); error_reporting(E_ALL); mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
@@ -62,57 +115,7 @@ $obj = new \ArPHP\I18N\Arabic();
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
 
-  <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
-<meta name="keywords" content="rondili, service, job, خدمة ,الجزائر ,عامل , رونديلي, بحث">
-<meta name="description" content="
-تبحث عن خدمة أو عامل في منطقتك ؟
-لديك عمل أو خدمة و تبحث عن الزبائن؟
-رونديلي هي الحل، منصة تجمع بين الزبون و أصحاب الأعمال rondili ">
-<meta name="author" content="rondili">
-    <meta charset="utf-8">
-    <title>Rondili - صفحة البحث</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Quicksand:300,500" rel="stylesheet">
-    <link rel="stylesheet" href="search.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-	          <script src="https://pagination.js.org/dist/2.1.5/pagination.js"></script>
-			   <script src="https://pagination.js.org/dist/2.1.5/pagination.min.js"></script>
-			   <script src="pagination-with-styles.js"></script>
-  <link href="//db.onlinewebfonts.com/c/7d411bb0357d6fd29347455b7d207995?family=JF+Flat" rel="stylesheet" type="text/css"/>
-
-  	<script src="data.js"></script>
-    <script data-ad-client="ca-pub-8433558651734014" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <script>
-
-
-function showResult(str) {
-  if (str.length==0) {
-    document.getElementById("livesearch").innerHTML="";
-    document.getElementById("livesearch").style.display="none";
-    return;
-  }
-  var xmlhttp=new XMLHttpRequest();
-  xmlhttp.onreadystatechange=function() {
-    if (this.readyState==4 && this.status==200) {
-      document.getElementById("livesearch").innerHTML=this.responseText;
-      document.getElementById("livesearch").style.border="1px solid #A5ACB2";
-      document.getElementById("livesearch").style.display="inline";
-    }
-  }
-  xmlhttp.open("GET","livesearch.php?q="+str,true);
-  xmlhttp.send();
-}
-
-function sugtoinput(x) {
-  document.getElementById('searchinput').value=x;
-}
-</script>
-
-  </head>
 
   <body>
 
